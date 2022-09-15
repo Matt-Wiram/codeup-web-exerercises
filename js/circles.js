f283cd717210e675ea722a4fdf2ef0a85fc716cc
Matthew Wiram <matthew.wiram551@gmail.com>
2022-09-15 16:16:59 -0400

    final

--- js/circles.js
+++ js/circles.js
@@ -15,12 +15,12 @@
         logInfo: function (doRounding) {
             // TODO: complete this method.
             if (doRounding) {
-               return Math.round(area)
+               return Math.round(this.radius)
             }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
         }
     };
 
@@ -32,7 +32,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+circle.radius = 5
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
